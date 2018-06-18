8acccc0f36a4ca4192d7af64e5c5dc510590d275
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2018-06-17 21:21:54 -0600

    added comments to code

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,16 @@
-let toggleNavStatus = false;
+let toggleNavStatus = false; // Sets variable to show that sidebar is closed
 
+// Function that toggles nav open and closed
 const toggleNav = function() {
+  // set variables that get HTML items by class and store them
   let getSidebar = document.querySelector('.nav-sidebar');
   let getSidebarList = document.querySelector('.nav-sidebar ul');
   let getSidebarTitle = document.querySelector('.nav-sidebar span');
   let getSidebarLinks = document.querySelectorAll('.nav-sidebar a');
 
+  // If navbar is closed, changes css sytling to open nav and show items
   if (toggleNavStatus === false) {
+    // Updates the style of the navbar
     getSidebarList.style.visibility = "visible";
     getSidebar.style.width = "272px";
     getSidebarTitle.style.opacity = "0.5";
@@ -15,10 +19,11 @@ const toggleNav = function() {
     for (let i = 0; i < arrayLength; i++) {
       getSidebarLinks[i].style.opacity = "1";
     }
-
+    // Sets the navbar status to open
     toggleNavStatus = true;
   }
 
+  // If navbar is open, changes css styling to close nav and hide items
   else if (toggleNavStatus === true) {
     getSidebar.style.width = "50px";
     getSidebarTitle.style.opacity = "0";
@@ -29,7 +34,7 @@ const toggleNav = function() {
     }
 
     getSidebarList.style.visibility = "hidden";
-
+    // Sets the navbar status to closed
     toggleNavStatus = false;
   }
 }
